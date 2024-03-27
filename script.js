c004149138ab20b2b6431649aaacc3e4769e1acc
Jyotishko Bhattacharjee <90274384+jyotishkob@users.noreply.github.com>
2024-03-27 09:19:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function fibonacci(num) {
 // your code here
-	let value=0;
-	let value2=1;
-	let nval;
-	for (let i=1; i<num; i++){
-		nval = value+value2;
-	}
-	console.log(nval)
+  let value=0;
+  let value2=1;
+  let nval;
+  for (let i=2; i<num; i++){
+    nval = value+value2;
+    value = value2
+    value2=nval
+  }
+  return nval
 }
+console.log(fibonacci(3))
 
 module.exports = fibonacci;
