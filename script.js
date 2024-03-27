e898c74ca63c431b358c2c7e4dd96cd5e8f9eb47
Jyotishko Bhattacharjee <90274384+jyotishkob@users.noreply.github.com>
2024-03-27 12:29:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ function fibonacci(num) {
   let value=0;
   let value2=1;
   let nval;
-	if (num == 0){
+	if (num <=1){
 		return 0;
-	}else if (num ==1){
-		return 1;
 	}else{
 		for (let i=2; i<num; i++){
 	    nval = value+value2;
